c1831d32e60208ad82f0cd1cc992daf7b936dff6
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-12-12 22:44:46 +0100

    comment out some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+// import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./modules/index.module.css";
 
@@ -35,7 +35,7 @@ export default function Home() {
       description={`${siteConfig.description}`}
     >
       <HomepageHeader />
-      <main>{/* <HomepageFeatures /> */}</main>
+      {/* <main><HomepageFeatures /> </main>*/}
     </Layout>
   );
 }
